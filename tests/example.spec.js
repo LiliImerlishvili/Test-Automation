01fe60747085068291c2455a2fab20cdb7cc2e59
“lilimelishvili” <lilimerlishvili@gmail.com>
2026-02-20 16:56:06 +0400

    add descriptive comment inside test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,5 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
+// This is an example test. You can run it with the command
 test('My first test', async ({ page }) => {
   await page.goto('https://example.com');
   await expect(page).toHaveTitle(/Example/);
