5c6155bcbb9bb763f9227f77f9768ee92663c69c
“lilimelishvili” <lilimerlishvili@gmail.com>
2026-02-20 19:32:06 +0400

    Creat first playwrighttest

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,13 @@
+const {test} = require('@playwright/test');
+
+
+test('First Playwright Test', async () => // async function to perform actions on the page = => arrow function
+{
+    //playwright code 
+    //step1 - open the browser
+    //step2 - open a new page
+    //step3 - navigate to the url
+    //step4 - perform actions on the page
+    //step5 - close the browser
+
+});
